52057fcac2d88ee044a3a3b01bdb3021a39dc993
ricidkumbara <ricidkumbara@gmail.com>
2024-08-08 13:50:32 +0700

    update

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	server := http.Server{
+		Handler: router,
+		Addr:    "localhost:3000",
+	}
+
+	server.ListenAndServe()
+}
